e348d50ed995fb57d0095e1d05d6c06da714a2da
Ogutu Brian <codingbrian58@gmail.com>
2019-01-16 13:29:01 +0300

    [Chore #163248100] Add more configuration to run.py

--- API/run.py
+++ API/run.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from app.api.v2.models.database import PostgresDatabase
+from instance.config import app_config
+
 database = PostgresDatabase()
-def create_app():
+
+
+def create_app(application_config):
+    """main flask application"""
     app = Flask(__name__)
+    app.config.from_object(app_config.get(application_config))
     return app
-app = create_app()
+
+
+app = create_app("DEVELOPMENT")
 if __name__ == "__main__":
     app.run()
