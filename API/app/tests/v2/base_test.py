3f88bcfb068837ae876211e443ed9a9a562d8a02
Ogutu Brian <codingbrian58@gmail.com>
2019-01-16 14:49:42 +0300

    [Chore #163248100] Add Base test class for the application

---
+++ API/app/tests/v2/base_test.py
@@ -0,0 +1,22 @@
+import unittest
+from run import create_app, database
+from migrtions import DbMigrations
+
+
+class BaseTest(unittest.TestCase):
+    """Defines a Base template class for performing unit testst"""
+
+    def setUp(self):
+        """Sets up a test client for the application"""
+        self.app = create_app("TESTING")
+        self.client = self.app.test_client
+        self.not_json_header = {}
+        self.json_headers = {"Content-Type": "application/json"}
+        self.url_prefix = "/api/v2"
+
+    def complete_url(self, url=""):
+        """Returns complete url endpoint that is tested by the view"""
+        return self.url_prefix+url
+
+    def tearDown(self):
+        DbMigrations.tear_down()
