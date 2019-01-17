347ff04ba9e5ec26596e221d9e035952ea2e704a
Ogutu Brian <codingbrian58@gmail.com>
2019-01-17 17:51:26 +0300

    [Finishes #163248409] Add more tests and enough code to pass them

--- api/tests/test_meetups.py
+++ api/tests/test_meetups.py
@@ -11,5 +11,28 @@ class TestMeetups(BaseTest):
     def test_successful_meetup_creation(self):
         """Tests for successful creation of a meetup"""
         result = self.create_meetup()
-        print(result)
+        self.assertEqual(Status.created, result.get("status"))
+
+    def test_missing_tags(self):
+        """Tests for data that does not contain tags"""
+        self.meetup_data.data["Tags"] = ""
+        result = self.create_meetup()
+        self.assertEqual(Status.invalid_data, result.get("status"))
+
+    def test_missing_location(self):
+        """tests for data that does not contain location of meetup"""
+        self.meetup_data.data["location"] = ""
+        result = self.create_meetup()
+        self.assertEqual(Status.invalid_data, result.get("status"))
+
+    def test_missing_meetup_date(self):
+        """Test data that does not contain meetup date"""
+        self.meetup_data.data["happeningOn"] = ""
+        result = self.create_meetup()
+        self.assertEqual(Status.invalid_data, result.get("status"))
+
+    def test_missing_images(self):
+        """Image location is optional so missing in json object should not cause failure"""
+        self.meetup_data.data["images"] = ""
+        result = self.create_meetup()
         self.assertEqual(Status.created, result.get("status"))
