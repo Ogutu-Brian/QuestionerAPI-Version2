3ea0aef8bdc9a14816044122a4f45f7c8d7b955e
Ogutu Brian <codingbrian58@gmail.com>
2019-01-17 15:27:30 +0300

    [Feature #163248409] Add meetup endpoint

--- api/app/views/meetup_views.py
+++ api/app/views/meetup_views.py
@@ -1,14 +1,36 @@
 from .import meetup_view
 from flask import request, jsonify
 from api.app.views import Status
-from api.app.models.object_models import Meetup
+from api.app.utils.validators import MeetupValidators
+
 
 @meetup_view.route("/meetups", methods=["POST"])
 def create_meetup():
     """A post endpoint for creating a meetup by an administrator"""
     response = None
     if request.is_json:
-        valid, errors =  M
+        data = request.json
+        valid, errors = MeetupValidators.is_valid(data)
+        if not valid:
+            response = jsonify({
+                "message": "You encountered {} errors".format(len(errors)),
+                "status": Status.invalid_data
+            }), Status.invalid_data
+        else:
+            from api.app.models.models import Meetup
+            location = data.get("location")
+            images = data.get("images")
+            topic = data.get("topic")
+            happening_on = data.get("happeningOn")
+            tags = data.get("Tags")
+            meetup = Meetup(location=location, images=images,
+                            topic=topic, happening_on=happening_on, tags=tags)
+            meetup.save()
+            response = jsonify({
+                "message": "Successfully created a meetup",
+                "data": [meetup.to_dictionary()],
+                "status": Status.created
+            }), Status.created
     else:
         response = jsonify({
             "message": "The data should be JSON",
